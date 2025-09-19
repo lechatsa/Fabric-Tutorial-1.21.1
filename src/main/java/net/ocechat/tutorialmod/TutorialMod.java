2b5dfcd4720fbe57df46bc368cb59b55d260e86c
Ocechat <lechatsa@gmail.com>
2025-09-19 22:53:56 +0200

    Simply follow the tutorial.

--- src/main/java/net/ocechat/tutorialmod/TutorialMod.java
+++ src/main/java/net/ocechat/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.ocechat.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ocechat.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,10 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+        ModItems.registerModItems();
+
+
 
 	}
 }
--- src/main/java/net/ocechat/tutorialmod/item/ModItems.java
+++ src/main/java/net/ocechat/tutorialmod/item/ModItems.java
@@ -1,4 +1,28 @@
 package net.ocechat.tutorialmod.item;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.ocechat.tutorialmod.TutorialMod;
+
 public class ModItems {
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+
+    }
+
+
+
+
+
+
+    public static void registerModItems() {
+
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+
+    }
+
 }
