d83491e08dca2b05cb62104969ddca0351c83167
Ocechat <lechatsa@gmail.com>
2025-09-20 12:00:51 +0200

    Simply follow the tutorial.

--- src/main/java/net/ocechat/tutorialmod/item/ModItems.java
+++ src/main/java/net/ocechat/tutorialmod/item/ModItems.java
@@ -8,21 +8,20 @@ import net.ocechat.tutorialmod.TutorialMod;
 
 public class ModItems {
 
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+
+
+
     private static Item registerItem(String name, Item item) {
+
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
 
     }
-
-
-
-
-
 
     public static void registerModItems() {
 
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
 
-
     }
-
 }
